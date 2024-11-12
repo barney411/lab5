efbe5c674b0e8d0ef6042bf46d3f307cc2d20aec
barney411 <sarahbarney@mail.weber.edu>
2024-11-12 14:33:22 -0700

    Add files via upload
    format tweaks

--- exercise7.c
+++ exercise7.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-
+//is_prime returns "true" on 1, "false" on 0 --- C Bool type kludge
 int is_prime(int i, int flag)
 {
-int j, prime_counter;
+int j, prime_counter; //may deprecate counter if not needed.
 
-// Loop to check if i is divisible by any number other than 1 and itself
+        // Loop to check if i is divisible by any number other than 1 and itself
         for (j = 2; j <= i / 2 && flag == 1; j++) 
         {
             if (i % j == 0) 
-            {
-                flag = 0;
+            {   
+                flag = 0; //NOT PRIME, Flag OFF
                 return 0;
             }
            
@@ -24,24 +24,23 @@ int j, prime_counter;
 
 }
 
-
-int main() {
+//MAIN
+int main() 
+{
     int i, j, flag, prime_counter = 0;
 
-    // Print a message indicating the purpose of the program
+    // Label
     printf("The prime numbers -between- 1 and 199 are:\n");
 
     // Loop to check prime numbers from 2 to 199
-    for (i = 2; i < 199; i++) {
+    for (i = 2; i < 199; i++) 
+    {
         
-        flag = 1; //2 is the smallest prime number
+        flag = 1; //2 is the smallest prime number, flag ON
         if (is_prime(i, flag))
         {
             printf("%3d is prime.\n", i);
         }
-    
-        
-
         
     }
     
--- exercise8.c
+++ exercise8.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
     int i;
 
     
-    for (i = 1; i < 18; i++) {
+    for (i = 1; i < 18; i++) 
+    {
         if(i%3!=0)
         {
             printf("Weber State!\n");
